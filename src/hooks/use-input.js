ac03aeac42fdc4b9185f4f63bec829d285f121c4
João Henrique <joaoenrique.san@gmail.com>
2022-01-14 17:56:26 -0300

    feat: finish section 16

---
+++ src/hooks/use-input.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
+
+  const enteredValueIsValid = validateValue(enteredValue);
+  const hasError = !enteredValueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue('');
+    setIsTouched(false);
+  };
+  return {
+    value: enteredValue,
+    hasError,
+    isValid: enteredValueIsValid,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
